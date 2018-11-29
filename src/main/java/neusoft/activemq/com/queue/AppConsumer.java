5fcc03a139ee033678761debbcd19c24cb2c401e
dmsansan <562404326@qq.com>
2018-11-29 14:42:34 +0800

    添加queue方式产生和消费中间件消息

---
+++ src/main/java/neusoft/activemq/com/queue/AppConsumer.java
@@ -0,0 +1,33 @@
+package neusoft.activemq.com.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.ConsumerBrokerExchange;
+
+import javax.jms.*;
+import javax.xml.soap.Text;
+
+public class AppConsumer {
+
+    private static final String url = "tcp://47.96.179.77:61616";
+    private static final String queueName="queue-test";
+
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createQueue(queueName);
+        MessageConsumer messageConsumer = session.createConsumer(destination);
+        messageConsumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                try {
+                    TextMessage textMessage = (TextMessage) message;
+                    System.out.println("接收消息：" + textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+//        connection.close();
+    }
+}
--- src/main/java/neusoft/activemq/com/queue/AppProducer.java
+++ src/main/java/neusoft/activemq/com/queue/AppProducer.java
@@ -1,5 +1,25 @@
 package neusoft.activemq.com.queue;
 
-public class AppProducer {
+import org.apache.activemq.ActiveMQConnectionFactory;
 
+import javax.jms.*;
+
+public class AppProducer {
+    private static final String url = "tcp://47.96.179.77:61616";
+    private static final String queueName="queue-test";
+
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createQueue(queueName);
+        MessageProducer producer = session.createProducer(destination);
+        for (int i = 0; i < 100; i++) {
+            TextMessage textMessage = session.createTextMessage("test"+i);
+            producer.send(textMessage);
+            System.out.println("发送的消息："+textMessage.getText());
+        }
+        connection.close();
+    }
 }
